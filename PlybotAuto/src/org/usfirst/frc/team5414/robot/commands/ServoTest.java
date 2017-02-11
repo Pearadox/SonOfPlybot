988eb7a88ac15e3c721a86250be122ebed081f6a
alljiang <alljiang@hotmail.com>
2017-02-11 14:20:50 -0600

    fgrdrfrgnhtrtfaetrtaertaeryrtudtyuuyttrdrysteRw3q4WAE5RTYUIUO

--- PlybotAuto/src/org/usfirst/frc/team5414/robot/commands/ServoTest.java
+++ PlybotAuto/src/org/usfirst/frc/team5414/robot/commands/ServoTest.java
@@ -35,7 +35,6 @@ public class ServoTest extends Command {
     	changed = true;
     }
 
-    // Make this return true when this Command no longer needs to run execute(
     protected boolean isFinished() {
         return changed;
     }
